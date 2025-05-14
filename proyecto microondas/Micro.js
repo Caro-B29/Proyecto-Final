204c5d1d4359b567acc07994c85ec6c180b9a7aa
deisy niño <deisycaro2904@gmail.com>
2025-05-14 15:31:56 -0500

    0.7

--- proyecto microondas/Micro.js
+++ proyecto microondas/Micro.js
@@ -18,3 +18,28 @@
 
    var marker = L.marker([4.61798365, -74.0612434411595]).addTo(map);
    
+   //abrir el archivo Geojson
+   //Javascript se ejecuta de tal manera que si una linea de codig se demora, se pasa  ala siguiente sin esperar. 
+   //Esto puede generar problemas cuando se requiere secuencialidad 
+
+   async function cargarpuntos(){
+   var miArchivo= await fetch("Micro.geojson");
+   //Convertir el contenido a json: objeto js
+   var datos= await miArchivo.json();
+   let listafeatures= datos["features"];
+   
+   listasfeature[0]
+   //Leer el archivo
+   console.log(datos);
+   for(let i=0;i<10;i++){
+    let miCoordenadas= listaFeature[i]["geometry"]["coordinates"];
+    var miMarcador= L.marker(misCoordenadas);
+    miMarcador.addTo(map);
+
+   }
+  
+
+
+   }
+   
+cargarpuntos();
