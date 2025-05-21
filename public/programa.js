4abbae4678a76b0a8cd99c380371dd2c7a6fdc38
deisy niño <deisycaro2904@gmail.com>
2025-05-21 15:16:27 -0500

    0.8

--- public/programa.js
+++ public/programa.js
@@ -16,4 +16,26 @@
    .bindPopup('Aquí está el lugar específico.')
    .openPopup();
 
-   
+// Añadir capa de mapa
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Crear un polígono para el barrio Marandú (por ejemplo, estas son coordenadas de muestra)
+const barrioMaranduPolygon = L.polygon([
+    [4.709085, -74.133390],
+    [4.707808, -74.133819],
+    [4.706818, -74.131052],
+    [4.704914, -74.132425],
+    [4.707224, -74.133804],
+    [4,705529, -74.133450],
+    [4.706858, -74.133553],
+    [4.707240, -74.133796],
+], {
+    color: 'blue',
+    fillColor: 'cyan',
+    fillOpacity: 0.5
+}).addTo(map);
+
+// Añadir título al mapa
+barrioMaranduPolygon.bindPopup('Barrio Marandú, Engativá').openPopup();
